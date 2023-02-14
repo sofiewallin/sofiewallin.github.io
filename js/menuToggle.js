ecc351a06c8d91d72c6e9c9ff43cb75df1ae7079
Sofie Wallin <sofie.wallin84@gmail.com>
2023-02-14 15:48:39 +0100

    Add script for mobile menu

---
+++ js/menuToggle.js
@@ -0,0 +1,22 @@
+const menuToggle = () => {
+    const toggleMenuButton = document.querySelector('.toggle-menu-button');
+    const mainMenu = document.querySelector('#main-menu');
+    
+    toggleMenuButton.addEventListener('click', e => {
+        mainMenu.classList.toggle('hidden');
+        toggleMenuButton.classList.toggle('hide');
+
+        if (toggleMenuButton.getAttribute('aria-expanded') === 'false') {
+            toggleMenuButton.setAttribute('aria-expanded', 'true');
+        } else {
+            toggleMenuButton.setAttribute( 'aria-expanded', 'false');
+        }
+
+        let hiddenTextElement = toggleMenuButton.querySelector('.hidden-visually');
+        if (hiddenTextElement.innerText === 'Show menu') {
+            hiddenTextElement.innerText = 'Hide menu';
+        } else {
+            hiddenTextElement.innerText = 'Show menu';
+        }
+    });
+}
